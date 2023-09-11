4792cc7b2ba404dfc948d6592a2fc474287ec305
Gürkan <gurkanyigitgr@gmail.com>
2023-09-11 17:38:54 +0300

    add solution

--- src/main/java/com/trendyol/shipment/Basket.java
+++ src/main/java/com/trendyol/shipment/Basket.java
@@ -1,13 +1,34 @@
 package com.trendyol.shipment;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Basket {
 
+    private static final int THRESHOLD_SIZE = 3;
     private List<Product> products;
 
     public ShipmentSize getShipmentSize() {
-        return null;
+        ShipmentSize largestSize = null;
+
+        int countOfLargestSize = 0;
+        for (Product product : products) {
+            ShipmentSize size = product.getSize();
+            if (largestSize == null || size.ordinal() > largestSize.ordinal()) {
+                largestSize = size;
+                countOfLargestSize = 1;
+            } else if (size.equals(largestSize)) {
+                countOfLargestSize++;
+            }
+        }
+
+        if (countOfLargestSize >= THRESHOLD_SIZE) {
+            return ShipmentSize.values()[largestSize.ordinal() + 1];
+        } else {
+            return largestSize;
+        }
     }
 
     public List<Product> getProducts() {
